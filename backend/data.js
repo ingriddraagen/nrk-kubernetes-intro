c4feb2699025807ce395cf093d3e70efb0c27642
Ingrid Gormsdatter Draagen <Ingrid.Draagen@nrk.no>
2020-05-19 11:51:33 +0200

    WIP

--- backend/data.js
+++ backend/data.js
@@ -1,36 +1,36 @@
 const education = [
-    {
-        yearFrom: 2005,
-        yearTo: 2007,
-        place: 'University 1',
-        comment: 'Studying'
-    },
-    {
-        yearFrom: 2001,
-        yearTo: 2004,
-        place: 'Barista academy',
-        comment: 'Made coffee'
-    }
-]
+  {
+    yearFrom: 1999,
+    yearTo: 2006,
+    place: "Sjetne Skole",
+    comment: "Barneskole",
+  },
+  {
+    yearFrom: 2006,
+    yearTo: 2009,
+    place: "Nidelven Skole",
+    comment: "Ungdomsskole",
+  },
+  {
+    yearFrom: 2009,
+    yearTo: 2012,
+    place: "KVT",
+    comment: "VGS",
+  },
+];
 
 const work = [
-    {
-        yearFrom: 2010,
-        yearTo: 2018,
-        place: 'BBC Radio',
-        comment: 'Was doing stuff'
-    },
-    {
-        yearFrom: 2009,
-        yearTo: 2010,
-        place: 'The coffee shop',
-        comment: 'Making coffee'
-    }
-]
+  {
+    yearFrom: 2020,
+    yearTo: 2020,
+    place: "NRK",
+    comment: "Frontend utvikler",
+  },
+];
 
 const me = {
-    name: 'Arthur Dent',
-    location: 'Earth'
-}
+  name: "Ingrid Gormsdatter Draagen",
+  location: "Oslo",
+};
 
 module.exports = { me, work, education };
